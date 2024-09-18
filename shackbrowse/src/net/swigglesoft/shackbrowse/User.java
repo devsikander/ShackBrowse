ad84a79cc327b1efc761072d1159b23120be6497
StimpyDad <stimpy.dad@doesntexist.com>
2024-09-17 20:18:32 -0400

    Update the User.java mod and employee list to lowercase so that it works properly. Setup release 7.1.1.

--- shackbrowse/src/net/swigglesoft/shackbrowse/User.java
+++ shackbrowse/src/net/swigglesoft/shackbrowse/User.java
@@ -5,8 +5,8 @@ import java.util.HashSet;
 
 public class User {
 
-    private final static HashSet<String> MODS = new HashSet<String>(Arrays.asList(new String[] { "AxeMan808", "Daeadin", "dognose", "EvilDolemite", "hirez", "Morgin", "Quix", "redfive", "Serpico74", "sikander", "Vincent Grayson" }));
-    private final static HashSet<String> EMPLOYEES = new HashSet<String>(Arrays.asList(new String[] { "the man with the briefcase", "SporkyReeve", "Daniel_Perez", "Joshua Hawkins", "Brittany Vincent", "beardedaxe", "GBurke59", "plonkus", "hammersuit", "staymighty", "shugamom" }));
+    private final static HashSet<String> MODS = new HashSet<String>(Arrays.asList(new String[] { "axeman808", "daeadin", "dognose", "evildolemite", "hirez", "morgin", "quix", "redfive", "serpico74", "sikander", "vincent grayson" }));
+    private final static HashSet<String> EMPLOYEES = new HashSet<String>(Arrays.asList(new String[] { "the man with the briefcase", "sporkyreeve", "daniel_perez", "joshua hawkins", "brittany vincent", "beardedaxe", "gburke59", "plonkus", "hammersuit", "staymighty", "shugamom" }));
 
     public static Boolean isEmployee(String userName)
     {
