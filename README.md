51ce3397844420140e807055f0c61ef9dc12a715
Sikander <sikander@webcode.ca>
2024-09-17 20:00:29 -0400

    Update User.java (#3)
    * Update User.java
    
    Updated, as of July 17, 2024
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # ShackBrowse
-https://play.google.com/store/apps/details?id=net.swigglesoft.shackbrowse
+ShackBrowse is an Android app for the Latest Chatty discussion forum on [https://www.shacknews.com/](Shacknews)
 
-Uses the Push Notification server from https://github.com/latestchatty/latest-chatty-uwp-push-notifications
+It can be installed from the Google Play store: [https://play.google.com/store/apps/details?id=net.swigglesoft.shackbrowse](https://play.google.com/store/apps/details?id=net.swigglesoft.shackbrowse)
+
+Notes
+------
+- ShackBrowse was forked from https://github.com/wogglenet/ShackBrowse in February 2023
+- ShackBrowse uses the Push Notification server from [https://github.com/latestchatty/latest-chatty-uwp-push-notifications](https://github.com/latestchatty/latest-chatty-uwp-push-notifications)
+- A Shacknews account is not necessary, but recommended to enable notifications, comment filters, direct messages, tags, etc.
--- shackbrowse/src/net/swigglesoft/shackbrowse/User.java
+++ shackbrowse/src/net/swigglesoft/shackbrowse/User.java
@@ -5,8 +5,8 @@ import java.util.HashSet;
 
 public class User {
 
-    private final static HashSet<String> MODS = new HashSet<String>(Arrays.asList(new String[] { "bitchesbecrazy",    "degenerate",    "EvilDolemite",    "hirez",    "Morgin",    "ninjase",    "Portax",    "redfive",    "Serpico74",    "thaperfectdrug" }));
-    private final static HashSet<String> EMPLOYEES = new HashSet<String>(Arrays.asList(new String[] { "the man with the briefcase", "SporkyReeve",    "Daniel_Perez",    "Joshua Hawkins",    "Brittany Vincent",    "beardedaxe",    "GBurke59",    "plonkus",    "hammersuit",    "staymighty", "shugamom" }));
+    private final static HashSet<String> MODS = new HashSet<String>(Arrays.asList(new String[] { "AxeMan808", "Daeadin", "dognose", "EvilDolemite", "hirez", "Morgin", "Quix", "redfive", "Serpico74", "sikander", "Vincent Grayson" }));
+    private final static HashSet<String> EMPLOYEES = new HashSet<String>(Arrays.asList(new String[] { "the man with the briefcase", "SporkyReeve", "Daniel_Perez", "Joshua Hawkins", "Brittany Vincent", "beardedaxe", "GBurke59", "plonkus", "hammersuit", "staymighty", "shugamom" }));
 
     public static Boolean isEmployee(String userName)
     {
